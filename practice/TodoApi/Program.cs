74ddb440e9fab08b113963945af5dd727bce8cbe
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-15 21:44:05 +1300

    Scaffold a controller #1

--- practice/TodoApi/Program.cs
+++ practice/TodoApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,14 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
+
+// <snippet_UseSwagger>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
+// </snippet_UseSwagger>
 
 app.UseHttpsRedirection();
 
