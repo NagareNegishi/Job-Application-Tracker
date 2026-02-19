87a5f85824f29a956b02f4a09591597180a43924
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-20 02:18:04 +1300

    Contacts and Correspondences do not need their own tables #2

--- JobTrackerApi/Data/JobTrackerContext.cs
+++ JobTrackerApi/Data/JobTrackerContext.cs
@@ -9,6 +9,18 @@ public class JobTrackerContext : DbContext
     {
     }
 
+    // Contacts and Correspondences do not need their own tables, they are owned by Job and stored as JSON
+    // While Complex type approach is possible in EF 10+, ComplexProperty maps a single nested object,
+    // not collections, so we use OwnsMany with ToJson() instead
+    // https://www.npgsql.org/efcore/mapping/json.html?tabs=data-annotations%2Ccomplex-types%2Cjsondocument
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Job>()
+            .OwnsMany(j => j.Contacts, contacts => contacts.ToJson());
+        modelBuilder.Entity<Job>()
+            .OwnsMany(j => j.Correspondences, correspondence => correspondence.ToJson());
+    }
+
     public DbSet<Job> TodoItems { get; set; } = null!;
     public DbSet<Document> Documents { get; set; } = null!;
 }
