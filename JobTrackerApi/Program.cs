ee49a628bf96eb708aa610114ffeee20e6c880ae
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-20 00:09:15 +1300

    register context #2

--- JobTrackerApi/Program.cs
+++ JobTrackerApi/Program.cs
@@ -1,3 +1,8 @@
+using JobTrackerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using JobTrackerApi.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// GetConnectionString method looks for a configuration value
+// https://learn.microsoft.com/en-us/ef/core/miscellaneous/connection-strings?tabs=dotnet-core-cli
+var connectionString = builder.Configuration.GetConnectionString("JobTrackerContext")
+    ?? throw new InvalidOperationException("Connection string 'JobTrackerContext' not found.");
+// Npgsql Entity Framework
+// https://www.npgsql.org/efcore/index.html?tabs=aspnet
+// No AddDbContextPool for safety and simplicity
+builder.Services.AddDbContext<JobTrackerContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+
+
+// <snippet_UseSwagger>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +37,7 @@ if (app.Environment.IsDevelopment())
         options.DocumentPath = "/openapi/v1.json";
     });
 }
+// <snippet_UseSwagger>
 
 app.UseHttpsRedirection();
 
