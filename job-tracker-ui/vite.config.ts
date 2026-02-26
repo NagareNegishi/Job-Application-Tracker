f52582ca9a4d16c165ac543b437b4e6b984c6a52
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-26 16:19:16 +1300

    setup tailwind #4

--- job-tracker-ui/vite.config.ts
+++ job-tracker-ui/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+// for tailwindcss
+import tailwindcss from '@tailwindcss/vite'
+// for shadcn/ui
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  // for shadcn/ui
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
 })
