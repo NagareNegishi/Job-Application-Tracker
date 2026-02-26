87018e11a3d7e44d60a34ec434111a150d74cfd2
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-26 21:52:48 +1300

    request type for job #4

--- job-tracker-ui/src/types/job.ts
+++ job-tracker-ui/src/types/job.ts
@@ -1,8 +1,8 @@
-// Job interface, mirrors JobResponseDto from the backend
 import type { Contact, Correspondence } from "./contact";
 import { JobStatus, Priority } from "./enums";
 import type { JobDocument } from "./jobDocument";
 
+// Job interface, mirrors JobResponseDto from the backend
 export interface Job {
     id: number;
     company: string;
@@ -19,5 +19,30 @@ export interface Job {
 }
 
 
-// Later
-// CreateJobRequest → mirrors CreateJobDTO
+// CreateJobRequest interface, mirrors JobDTO from the backend
+export interface CreateJobRequest {
+    company: string;
+    role: string;
+    status: JobStatus;
+    priority?: Priority;
+    appliedAt?: string; // ISO date string
+    closedAt?: string; // ISO date string
+    description?: string;
+    notes?: string;
+    contacts?: Contact[];
+}
+
+
+// UpdateJobRequest interface, mirrors UpdateJobDTO from the backend
+export interface UpdateJobRequest {
+    company: string;
+    role: string;
+    status: JobStatus;
+    priority?: Priority;
+    appliedAt?: string; // ISO date string
+    closedAt?: string; // ISO date string
+    description?: string;
+    notes?: string;
+    contacts?: Contact[];
+    correspondences?: Correspondence[];
+}
