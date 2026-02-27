f94c1a6df3b7ddd52fe34d987b9daa8ce27d593d
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-28 02:37:41 +1300

    routing setup #4

--- job-tracker-ui/src/main.tsx
+++ job-tracker-ui/src/main.tsx
@@ -2,17 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 // TanStack Query Setup, it requires to wrap the app with QueryClientProvider
 // https://tanstack.com/query/v5/docs/framework/react/quick-start
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// React Router Setup, it requires to wrap the app with BrowserRouter
+// https://reactrouter.com/start/declarative/installation
+import { BrowserRouter } from "react-router"
 
 // Create a client
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
