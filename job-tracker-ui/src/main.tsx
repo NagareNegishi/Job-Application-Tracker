2127b8640590bbabb0be7d4cd37a7970cc9e5ce4
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-27 22:02:09 +1300

    TanStack Query Setup #4

--- job-tracker-ui/src/main.tsx
+++ job-tracker-ui/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+// TanStack Query Setup, it requires to wrap the app with QueryClientProvider
+// https://tanstack.com/query/v5/docs/framework/react/quick-start
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
