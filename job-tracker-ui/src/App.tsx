0fdfe96ea032a3517c1a6469aaf7ad94c8778ba1
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-27 23:11:32 +1300

    refactor table should handle job data #4

--- job-tracker-ui/src/App.tsx
+++ job-tracker-ui/src/App.tsx
@@ -1,17 +1,11 @@
-import {
-  useQuery
-} from '@tanstack/react-query'
 import { JobTable } from './components/jobTable'
-import { getJobs } from './services/jobService'
 
 function App() {
-  // jobs list
-  const { data: jobs } = useQuery({ queryKey: ["jobs"], queryFn: getJobs })
 
   return (
     <div className="App">
       {/* <p className="text-2xl font-bold mb-4">Job Tracker</p> */}
-      <JobTable jobs={jobs ?? []} />
+      <JobTable />
     </div>
   )
 }
--- job-tracker-ui/src/components/jobTable.tsx
+++ job-tracker-ui/src/components/jobTable.tsx
@@ -7,64 +7,37 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { cn } from "@/lib/utils";
-import type { Job } from "../types/job";
-// import type { Contact, Correspondence } from "../types/contact";
-// import { JobStatus, Priority } from "../types/enums";
-// import type { JobDocument } from "../types/jobDocument";
+import { useJobs } from "@/hooks/jobQuery";
 
-
-interface JobTableProps {
-  jobs: Job[];
-  // Optional className for styling
-  className?: string;
-}
-
-// // Job interface, mirrors JobResponseDto from the backend
-// export interface Job {
-//   id: number;
-//   company: string;
-//   role: string;
-//   status: JobStatus;
-//   priority?: Priority;
-//   appliedAt?: string; // ISO date string
-//   closedAt?: string; // ISO date string
-//   documents?: JobDocument[]; // Don't expose internal Document
-//   description?: string;
-//   notes?: string;
-//   contacts?: Contact[];
-//   correspondences?: Correspondence[];
-// }
-
-
-export function JobTable({ jobs, className }: JobTableProps) {
+export function JobTable() {
+  const { data: jobs, isPending, isError } = useJobs()
+  if (isPending) return <p>Loading...</p>
+  if (isError) return <p>Something went wrong.</p>
   return (
-    <div className={cn(className)}>
-      <Table>
-        <TableCaption>Some Caption</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Company</TableHead>
-            <TableHead>Role</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Priority</TableHead>
-            <TableHead>Applied At</TableHead>
-            <TableHead>Closed At</TableHead>
+    <Table>
+      <TableCaption>Some Caption</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Company</TableHead>
+          <TableHead>Role</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Priority</TableHead>
+          <TableHead>Applied At</TableHead>
+          <TableHead>Closed At</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {jobs.map((job) => (
+          <TableRow key={job.id}>
+            <TableCell className="font-medium">{job.company}</TableCell>
+            <TableCell>{job.role}</TableCell>
+            <TableCell>{job.status}</TableCell>
+            <TableCell>{job.priority ?? "N/A"}</TableCell>
+            <TableCell>{job.appliedAt ? new Date(job.appliedAt).toLocaleDateString() : "N/A"}</TableCell>
+            <TableCell>{job.closedAt ? new Date(job.closedAt).toLocaleDateString() : "N/A"}</TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {jobs.map((job) => (
-            <TableRow key={job.id}>
-              <TableCell className="font-medium">{job.company}</TableCell>
-              <TableCell>{job.role}</TableCell>
-              <TableCell>{job.status}</TableCell>
-              <TableCell>{job.priority ?? "N/A"}</TableCell>
-              <TableCell>{job.appliedAt ? new Date(job.appliedAt).toLocaleDateString() : "N/A"}</TableCell>
-              <TableCell>{job.closedAt ? new Date(job.closedAt).toLocaleDateString() : "N/A"}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
