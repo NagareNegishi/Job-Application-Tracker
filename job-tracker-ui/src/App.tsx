cb482191ad53e5dd0b83c39f207eb91dbc71ccc5
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-02-28 15:13:12 +1300

    redirect to job #4

--- job-tracker-ui/src/App.tsx
+++ job-tracker-ui/src/App.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router"
+import { Navigate, Route, Routes } from "react-router"
 import JobPage from "./pages/JobPage"
 
 function App() {
 
   return (
     <Routes>
-      {/* change to /jobs */}
-      <Route path="/" element={<JobPage />} />
+      {/* Redirect the root path to /jobs */}
+      <Route path="/" element={<Navigate to="/jobs" replace />} />
+      <Route path="/jobs" element={<JobPage />} />
     </Routes>
   )
 }
