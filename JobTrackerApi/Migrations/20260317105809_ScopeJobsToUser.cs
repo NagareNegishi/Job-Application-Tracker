895e407efbd4a3c0ff1d194fdcca01cfd359f3a0
negishnaga <negishnaga@ecs.vuw.ac.nz>
2026-03-17 10:59:17 +0000

    migrate and update db #16

---
+++ JobTrackerApi/Migrations/20260317105809_ScopeJobsToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScopeJobsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Jobs",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Jobs");
+        }
+    }
+}
