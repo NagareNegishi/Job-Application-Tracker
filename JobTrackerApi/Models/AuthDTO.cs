5c7a506635b32b9fdb23f7b8f30847d59f56f269
Nagare Negishi <negishinaga@gmail.com>
2026-04-16 04:48:56 +0000

    Add annotations #65

--- JobTrackerApi/Models/AuthDTO.cs
+++ JobTrackerApi/Models/AuthDTO.cs
@@ -1,51 +1,72 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace JobTrackerApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 /// <summary>
 /// DTO for register/ login user
 /// </summary>
 public class RegisterDTO
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
+
+    [Required]
+    [StringLength(ValidationConstants.MaxPasswordLength, MinimumLength = ValidationConstants.MinPasswordLength)]
     public string Password { get; set; } = "";
 }
 
 public class LoginDTO
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
+
+    [Required]
     public string Password { get; set; } = "";
 }
 
 public class ResendConfirmationDTO
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
 }
 
 public class ChangePasswordDTO
 {
     [Required]
-    [StringLength(128)]
+    [StringLength(ValidationConstants.MaxPasswordLength)]
     public string CurrentPassword { get; set; } = "";
 
     [Required]
-    [StringLength(128, MinimumLength = 8)]
+    [StringLength(ValidationConstants.MaxPasswordLength, MinimumLength = ValidationConstants.MinPasswordLength)]
     public string NewPassword { get; set; } = "";
 
     [Required]
+    [Compare("NewPassword")]
     public string ConfirmNewPassword { get; set; } = "";
 }
 
 // Forgot password — request a reset link
 public class ForgotPasswordDTO
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
 }
 
 // Reset password — submitted from the link in the reset email
 public class ResetPasswordDTO
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = "";
+
+    [Required]
+    [StringLength(ValidationConstants.MaxTokenLength)]
     public string Token { get; set; } = "";
+
+    [Required]
+    [StringLength(ValidationConstants.MaxPasswordLength, MinimumLength = ValidationConstants.MinPasswordLength)]
     public string NewPassword { get; set; } = "";
 }
